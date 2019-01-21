ac0e49187f2da23fe51100ad6ba61d9f40061821
somania <somanianurmahal@gmail.com>
2019-01-21 12:46:06 +0600

    changes from local mac

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = true
+  # config.active_record.dump_schema_migration = true
 end
